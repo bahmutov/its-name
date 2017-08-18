d96f0b53b6f75163c9227197ca5618db5d2f4b5e
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-08-18 14:42:07 -0400

    initial code

---
+++ src/its-name-spec.js
@@ -0,0 +1,11 @@
+'use strict'
+
+
+/* eslint-env mocha */
+const itsName = require('.')
+
+describe('its-name', () => {
+  it('write this test', () => {
+    console.assert(itsName, 'should export something')
+  })
+})
